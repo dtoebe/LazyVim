58517f06765a8b3a3f89155a3f44ebf2b85402c8
Daniel Toebe <dtoebe@protonmail.com>
2026-02-05 08:14:26 -0700

    fix: formatting

--- init.lua
+++ init.lua
@@ -19,14 +19,14 @@ vim.opt.signcolumn = "yes"
 -- Install lazy.nvim (plugin manager) if not present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -37,34 +37,37 @@ require("config.autocmds")
 
 -- Plugin setup with lazy.nvim
 require("lazy").setup({
-  -- 1) LazyVim core and extras
-  {
-    "LazyVim/LazyVim",
-    import = "lazyvim.plugins",
-  },
+	-- 1) LazyVim core and extras
+	{
+		"LazyVim/LazyVim",
+		import = "lazyvim.plugins",
+	},
 
-  -- Language / feature extras (LazyVim modules)
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.python" }, -- helper tools / devcontainers
-  { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+	-- Language / feature extras (LazyVim modules)
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.python" }, -- helper tools / devcontainers
+	{ import = "lazyvim.plugins.extras.dap.core" },
+	{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.util.rest" }, -- ← add this[web:202]
 
-  -- 2) Your plugins (local)
-  { import = "plugins.core" },
-  { import = "plugins.go" },
-  { import = "plugins.flutter" },
-  { import = "plugins.js_ts" },
-  { import = "plugins.csharp" },
-  { import = "plugins.git" },
-  { import = "plugins.devops" },
+	-- 2) Your plugins (local)
+	{ import = "plugins.core" },
+	{ import = "plugins.go" },
+	{ import = "plugins.flutter" },
+	{ import = "plugins.js_ts" },
+	{ import = "plugins.csharp" },
+	{ import = "plugins.git" },
+	{ import = "plugins.devops" },
+	{ import = "plugins.flash" }, -- ← add this so your flash.lua override is loaded
 }, {
-  defaults = {
-    lazy = true,
-  },
-  checker = {
-    enabled = false,
-  },
+	defaults = {
+		lazy = true,
+	},
+	checker = {
+		enabled = false,
+	},
 })
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,119 +1,124 @@
 -- ~/.config/nvim/lua/plugins/core.lua
 return {
-  -- Dracula theme
-  {
-    "Mofiqul/dracula.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("dracula")
-    end,
-  },
+	-- Dracula theme
+	{
+		"Mofiqul/dracula.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
 
-  -- Mason from mason-org
-  {
-    "mason-org/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
-  },
+	-- Mason from mason-org
+	{
+		"mason-org/mason.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate",
+		opts = {
+			ui = {
+				border = "rounded",
+			},
+		},
+	},
 
-  -- mason-lspconfig: ensure LSP servers
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = { "mason-org/mason.nvim" },
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
+	-- mason-lspconfig: ensure LSP servers
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = { "mason-org/mason.nvim" },
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
 
-      local servers = {
-        "gopls",
-        "tsserver",
-        "eslint",
-        "jsonls",
-        "lua_ls",
-        "bashls",
-        "yamlls",
-        "dockerls",
-        "docker_compose_language_service",
-        "helm_ls",
-        "omnisharp",
-      }
+			local servers = {
+				"gopls",
+				"tsserver",
+				"eslint",
+				"jsonls",
+				"lua_ls",
+				"bashls",
+				"yamlls",
+				"dockerls",
+				"docker_compose_language_service",
+				"helm_ls",
+				"omnisharp",
+				"markdown_oxide",
+				"pyright",
+				"ruff_lsp",
+			}
 
-      for _, s in ipairs(servers) do
-        if not vim.tbl_contains(opts.ensure_installed, s) then
-          table.insert(opts.ensure_installed, s)
-        end
-      end
-    end,
-  },
+			for _, s in ipairs(servers) do
+				if not vim.tbl_contains(opts.ensure_installed, s) then
+					table.insert(opts.ensure_installed, s)
+				end
+			end
+		end,
+	},
 
-  -- mason-null-ls: formatters & linters
-  {
-    "jay-babu/mason-null-ls.nvim",
-    dependencies = { "mason-org/mason.nvim", "nvimtools/none-ls.nvim" },
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
+	-- mason-null-ls: formatters & linters
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = { "mason-org/mason.nvim", "nvimtools/none-ls.nvim" },
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
 
-      local tools = {
-        "golangci-lint",
-        "goimports",
-        "gofumpt",
-        "stylua",
-        "prettier",
-        "eslint_d",
-        "hadolint",
-        "yamllint",
-        "jsonlint",
-        "markdownlint",
-      }
+			local tools = {
+				"golangci-lint",
+				"goimports",
+				"gofumpt",
+				"stylua",
+				"prettier",
+				"eslint_d",
+				"hadolint",
+				"yamllint",
+				"jsonlint",
+				"markdownlint",
+				"black",
+				"ruff",
+				"isort",
+			}
 
-      for _, t in ipairs(tools) do
-        if not vim.tbl_contains(opts.ensure_installed, t) then
-          table.insert(opts.ensure_installed, t)
-        end
-      end
-    end,
-  },
+			for _, t in ipairs(tools) do
+				if not vim.tbl_contains(opts.ensure_installed, t) then
+					table.insert(opts.ensure_installed, t)
+				end
+			end
+		end,
+	},
 
-  -- Treesitter global base (languages added in language files)
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      local base = {
-        "lua",
-        "bash",
-        "json",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "dockerfile",
-        "toml",
-        "regex",
-        "vim",
-        "vimdoc",
-      }
-      for _, lang in ipairs(base) do
-        if not vim.tbl_contains(opts.ensure_installed, lang) then
-          table.insert(opts.ensure_installed, lang)
-        end
-      end
-    end,
-  },
+	-- Treesitter global base (languages added in language files)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			local base = {
+				"lua",
+				"bash",
+				"json",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"dockerfile",
+				"toml",
+				"regex",
+				"vim",
+				"vimdoc",
+			}
+			for _, lang in ipairs(base) do
+				if not vim.tbl_contains(opts.ensure_installed, lang) then
+					table.insert(opts.ensure_installed, lang)
+				end
+			end
+		end,
+	},
 
-  -- Telescope keymaps
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
-    },
-  },
+	-- Telescope keymaps
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+		},
+	},
 }
-
